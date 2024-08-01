e328c60ade060bebe708dd9fb501c83a5440c561
Alessandro Pirovano <a.pirovano@lutech.it>
2024-08-01 17:55:44 +0200

    Aggiungere i file di progetto.

---
+++ QAction_1010/QAction_1010.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Skyline.DataMiner.Scripting;
+
+/// <summary>
+/// DataMiner QAction Class: QACountUp.
+/// </summary>
+public static class QAction
+{
+	/// <summary>
+	/// The QAction entry point.
+	/// </summary>
+	/// <param name="protocol">Link with SLProtocol process.</param>
+	public static void Run(SLProtocol protocol)
+	{
+		try
+		{
+            int count = Convert.ToInt32(protocol.GetParameter(10));
+            count++;
+            protocol.SetParameter(10, count);
+
+            protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Count is {count}", LogType.Error, LogLevel.NoLogging);
+
+        }
+        catch (Exception ex)
+		{
+			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown:{Environment.NewLine}{ex}", LogType.Error, LogLevel.NoLogging);
+		}
+	}
+}
---
+++ QAction_1020/QAction_1020.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Skyline.DataMiner.Scripting;
+
+/// <summary>
+/// DataMiner QAction Class: QACountDown.
+/// </summary>
+public static class QAction
+{
+    /// <summary>
+    /// The QAction entry point.
+    /// </summary>
+    /// <param name="protocol">Link with SLProtocol process.</param>
+    public static void Run(SLProtocol protocol)
+    {
+        try
+        {
+            int count = Convert.ToInt32(protocol.GetParameter(10));
+            if (count > 0)
+            {
+                count--;
+                protocol.SetParameter(10, count);
+            }
+
+            protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Count is {count}", LogType.Error, LogLevel.NoLogging);
+
+        }
+        catch (Exception ex)
+        {
+            protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown:{Environment.NewLine}{ex}", LogType.Error, LogLevel.NoLogging);
+        }
+    }
+}
